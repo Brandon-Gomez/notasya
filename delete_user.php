cd91d4357aed59c6da8ed25b4e3a37f8f3594acb
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-12-19 16:54:53 -0500

    bien

--- delete_user.php
+++ delete_user.php
@@ -7,12 +7,17 @@
 
         $id = $_POST['delete_id'];
       
-        //registrar nuevo
+        //Error por llave foranea 
+        $sql="SET FOREIGN_KEY_CHECKS=0";
+        $resultado = mysqli_query($con,$sql);
+
+        //BORRA USER
         $consulta = "DELETE FROM usuarios WHERE id ='$id'";
         $resultado = mysqli_query($con,$consulta); 
 
         if ($resultado) {
-            header("location:user.php");   
+            
+            header("location:user.php");  
         }
         else {
             echo'<script type="text/javascript">
